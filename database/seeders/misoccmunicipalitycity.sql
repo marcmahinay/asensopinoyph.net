140289066b3de16915785cb7136942aeabc7c2b8
marcmahinay <marc.mahinay@gmail.com>
2024-09-02 09:01:50 +0800

    sql insert for provinces, misocc cities and municipalities and misocc barangays

---
+++ database/seeders/misoccmunicipalitycity.sql
@@ -0,0 +1,18 @@
+INSERT INTO municities (name, province_id) VALUES
+('Aloran', 55),
+('Baliangao', 55),
+('Bonifacio', 55),
+('Calamba', 55),
+('Clarin', 55),
+('Concepcion', 55),
+('Don Victoriano Chiongbian', 55),
+('Jimenez', 55),
+('Lopez Jaena', 55),
+('Oroquieta City', 55),
+('Ozamiz City', 55),
+('Panaon', 55),
+('Plaridel', 55),
+('Sapang Dalaga', 55),
+('Sinacaban', 55),
+('Tangub City', 55),
+('Tudela', 55);
