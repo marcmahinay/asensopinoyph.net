c2fc33b968ae51f423a5ae643b6690214b496a2c
marcmahinay <marc.mahinay@gmail.com>
2024-09-02 09:15:08 +0800

    changes : - logo height size - change href /dashboard - change main nav Localities - change href /provinces - change href /municities - change href /barangays - change icon heart for assistance - change icon tag for voucher - change icon users for Users

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,10 +5,10 @@
         <!-- Dark Logo-->
         <a href="index.html" class="logo logo-dark">
             <span class="logo-sm">
-                <img src="assets/images/logo-sm.png" alt="" height="22">
+                <img src="assets/images/logo-sm.png" alt="" height="40">
             </span>
             <span class="logo-lg">
-                <img src="assets/images/logo-dark.png" alt="" height="17">
+                <img src="assets/images/logo-dark.png" alt="" height="70">
             </span>
         </a>
         <!-- Light Logo-->
@@ -33,7 +33,7 @@
             <ul class="navbar-nav" id="navbar-nav">
                 <li class="menu-title"><span data-key="t-menu">Menu</span></li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sidebarDashboards">
+                    <a class="nav-link menu-link" href="/dashboard">
                         <i data-feather="home" class="icon-dual"></i> <span data-key="t-dashboards">Dashboards</span>
                     </a>
                 </li> <!-- end Dashboard Menu -->
@@ -41,25 +41,25 @@
                     <a class="nav-link menu-link" href="#sidebarApps">
                         <i data-feather="grid" class="icon-dual"></i> <span data-key="t-apps">Beneficiaries</span>
                     </a>
-                    
+
                 </li>
 
-                
+
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="#sidebarApps" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarApps">
-                        <i data-feather="grid" class="icon-dual"></i> <span data-key="t-apps">Locations</span>
+                        <i data-feather="map-pin" class="icon-dual"></i> <span data-key="t-apps">Localities</span>
                     </a>
                     <div class="collapse menu-dropdown" id="sidebarApps">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item">
-                                <a href="apps-calendar.html" class="nav-link" data-key="t-calendar"> Proince </a>
+                                <a href="/provinces" class="nav-link" data-key="t-calendar"> Province </a>
                             </li>
                             <li class="nav-item">
-                                <a href="apps-chat.html" class="nav-link" data-key="t-chat"> Municipality / City </a>
+                                <a href="/municities" class="nav-link" data-key="t-chat"> Municipality / City </a>
                             </li>
-                            
+
                             <li class="nav-item">
-                                <a href="apps-api-key.html" class="nav-link" data-key="t-api-key">Barangay</a>
+                                <a href="/barangays" class="nav-link" data-key="t-api-key">Barangay</a>
                             </li>
                         </ul>
                     </div>
@@ -77,7 +77,7 @@
                             <li class="nav-item">
                                 <a href="apps-chat.html" class="nav-link" data-key="t-chat"> Assis </a>
                             </li>
-                            
+
                             <li class="nav-item">
                                 <a href="apps-api-key.html" class="nav-link" data-key="t-api-key">Barangay</a>
                             </li>
@@ -87,24 +87,24 @@
 
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="#sidebarLayouts" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
-                        <i data-feather="layout" class="icon-dual"></i> <span data-key="t-layouts">Assistance</span> <span class="badge badge-pill bg-danger" data-key="t-hot">Hot</span>
+                        <i data-feather="heart" class="icon-dual"></i> <span data-key="t-layouts">Assistance</span> <span class="badge badge-pill bg-danger" data-key="t-hot">Hot</span>
                     </a>
                 </li> <!-- end Dashboard Menu -->
 
 
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="#sidebarLayouts" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
-                        <i data-feather="layout" class="icon-dual"></i> <span data-key="t-layouts">Voucher</span> <span class="badge badge-pill bg-danger" data-key="t-hot">Hot</span>
+                        <i data-feather="tag" class="icon-dual"></i> <span data-key="t-layouts">Voucher</span> <span class="badge badge-pill bg-danger" data-key="t-hot">Hot</span>
                     </a>
                 </li> <!-- end Dashboard Menu -->
-               
+
 
 
                 <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-components">Administration</span></li>
 
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="#sidebarUI">
-                        <i data-feather="package" class="icon-dual"></i> <span data-key="t-base-ui">Users</span>
+                        <i data-feather="users" class="icon-dual"></i> <span data-key="t-base-ui">Users</span>
                     </a>
             </ul>
         </div>
