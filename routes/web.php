4327fe7ea1d56b92ee3355d824380aadfe212b91
marcmahinay <marc.mahinay@gmail.com>
2024-09-02 09:15:46 +0800

    add routes for provinces add routes for municities add routes for barangays

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProvinceController;
+use App\Http\Controllers\MunicityController;
+use App\Http\Controllers\BarangayController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +18,10 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('provinces', ProvinceController::class);
+    Route::resource('municities', MunicityController::class);
+    Route::resource('barangays', BarangayController::class);
 });
 
 require __DIR__.'/auth.php';
